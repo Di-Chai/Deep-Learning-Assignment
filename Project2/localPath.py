cd7de6ad9a9638cba196da187f87dae140870359
Di-Chai <31341602+Di-Chai@users.noreply.github.com>
2017-11-23 23:57:20 +0800

    Add files via upload

---
+++ Project2/localPath.py
@@ -0,0 +1,10 @@
+import os
+
+projectPath = os.path.dirname(os.path.abspath(__file__))
+
+dataPath = os.path.join(projectPath, 'pic/')
+
+logPath = os.path.join(projectPath, 'data/')
+
+logPath2 = os.path.join(projectPath, 'data2/')
+
